dba3704934aac3777a630f521d9ae6e968c507c1
d-smith <doug.smith.mail@gmail.com>
2022-12-16 15:21:33 -0800

    added send ether, ownership test, onlyOwner modifier

--- contracts/scsender.sol
+++ contracts/scsender.sol
@@ -3,12 +3,29 @@
 pragma solidity ^0.8.13;
 
 contract EtherSender {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner may call this function");
+        _;
+    }
+
     receive() external payable {}
 
     fallback() external payable {}
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
+    }
+
+    function sendEther(uint val, address to) onlyOwner() public{
+         (bool sent, ) = to.call{value:val}("");
+        require(sent, "Failed to send Ether");
     }
 
     
--- test/scsend.js
+++ test/scsend.js
@@ -18,11 +18,19 @@ const { ethers } = require("hardhat");
   
       const EtherSender = await ethers.getContractFactory("EtherSender");
       const etherSender = await EtherSender.deploy();
+      await etherSender.deployed();
   
       return { etherSender, owner, acct1, acct2 };
     }
   
     describe("Deployment", function () {
+
+      it("Should set the right owner", async function () {
+
+        const { etherSender, owner } = await loadFixture(deloyEtherContract);
+        expect(await etherSender.owner()).to.equal(owner.address);
+      });
+
       it("Should set the right initial balance", async function () {
         const { etherSender, owner, acct1, acct2  } = await loadFixture(deloyEtherContract);
   
@@ -41,6 +49,28 @@ const { ethers } = require("hardhat");
         
       });
 
+      
+      it("Should allowing sending and show updated balance", async function () {
+        const { etherSender, owner, acct1, acct2  } = await loadFixture(deloyEtherContract);
+       
+        await owner.sendTransaction({
+            to: etherSender.address,
+            value: ethers.utils.parseEther("2")
+        });
+
+        expect(await etherSender.getBalance()).to.equal(ethers.utils.parseEther("2"));
+
+        let startBalance = await ethers.provider.getBalance(acct1.address);
+
+        await etherSender.connect(owner).sendEther(ethers.utils.parseEther("1"), acct1.address);
+
+        expect(await etherSender.getBalance()).to.equal(ethers.utils.parseEther("1"));
+        let endBalance = await ethers.provider.getBalance(acct1.address);
+        expect(endBalance.sub(startBalance)).to.equal(ethers.utils.parseEther("1"))
+        
+        
+      });
+
 
   
 
@@ -48,4 +78,5 @@ const { ethers } = require("hardhat");
   
     
   });
+
   
